e69eda21b0e30d486b239d9d4f3cc73d2f28197f
Tairikuookami <graywolf@noreply.192.168.114.112>
2024-09-15 11:17:12 +0000

    Removed unnecessary uints

--- mdlstream.lua
+++ mdlstream.lua
@@ -17,15 +17,13 @@
 MDLStream = {}
 
 --- Shared konstants
-local max_msg_size         = 64000 - 3 - 1 - 3 - 3 - 3 - 3 - 3  -- bytes, 0.063 MB, around 100 msgs to transmit a 8 MB file
+local max_msg_size         = 64000 - 3 - 1 - 3 - 3 - 3  -- bytes, 0.063 MB, around 100 msgs to transmit a 8 MB file
 -- 3 spared for engine use
 -- 1 for determining the response mode
 -- #content for the actual partial(sliced) compressed string of byte sequence of target file
 -- 3 for #content(slice) length
 -- 3 for #content slice ending position
--- 3 for uid of every accepted request
--- 3 for uid of content clientside
--- 3 for uid of content path
+-- 3 for uid of every accepted request, generated on client
 
 local size_3bytes = 24
 -- every uint we read and write will be a 24-bit one, max = 16777215
@@ -49,9 +47,6 @@ if CLIENT then
 
     local file_formats = {["mdl"] = true, ["vvd"] = true, ["phy"] = true}
 
-    local errorstr_unsupported_format = "MDLStream: Tries to send unsupported file, "
-    local errorstr_size_abnormal      = "MDLStream: Tries to send file larger than 8 MB, "
-
     local netlib_wstring  = net.WriteString
     local netlib_toserver = net.SendToServer
 
@@ -61,7 +56,7 @@ if CLIENT then
         net.WriteData(_data, _len)
     end
 
-    local content_uid  = content_uid or 1 -- included in msg
+    local uid  = uid or 1 -- included in msg
     -- To ensure that we don't lose the identity of one file's content when this client request to send another
     -- which leads to overriding of file content
 
@@ -87,42 +82,41 @@ if CLIENT then
 
     local function send_request(path)
         if not file_formats[string.GetExtensionFromFilename(path)] then
-            ErrorNoHalt(errorstr_unsupported_format, path)
+            ErrorNoHalt("MDLStream: Tries to send unsupported file, ", path)
 
             return false
         end
 
         if file.Size(path, "GAME") > max_file_size then
-            ErrorNoHalt(errorstr_size_abnormal, path)
+            ErrorNoHalt("MDLStream: Tries to send file larger than 8 MB, ", path)
 
             return false
         end
 
-        content_uid = content_uid + 1
+        uid = uid + 1
+        content_temp[uid] = {[1] = "", [2] = path}
 
         netlib_start("mdlstream_request")
         netlib_wstring(path)
-        netlib_wuint(content_uid)
+        netlib_wuint(uid)
         netlib_toserver()
-
-        content_temp[content_uid] = lzma(serialize_table(bytes_table(path)))
 
         return true
     end
 
     netlib_set_receiver("mdlstream_svblink", function()
-        local blink_mode   = netlib_rbool()
-        local uid          = netlib_ruint()
+        local blink_mode = netlib_rbool()
+        local _uid       = netlib_ruint()
 
         netlib_start("mdlstream_slice")
 
-        netlib_wuint(uid)
+        netlib_wuint(_uid)
 
         --- May better simplify section below
-        local _content_uid
         if blink_mode == false then
-            _content_uid   = netlib_ruint()
-            local _content = content_temp[_content_uid]
+            content_temp[_uid][1] = lzma(serialize_table(bytes_table(content_temp[_uid][2])))
+
+            local _content = content_temp[_uid][1]
 
             local exceeds_max = #_content > max_msg_size
             netlib_wbool(exceeds_max)
@@ -135,10 +129,10 @@ if CLIENT then
                 netlib_wuint(max_msg_size)
             end
         elseif blink_mode == true then
-            local pos    = netlib_ruint()
-            _content_uid = netlib_ruint()
+            local _content = content_temp[_uid][1]
 
-            local _content = content_temp[_content_uid]
+            local pos    = netlib_ruint()
+
             local exceeds_max = #_content - pos > max_msg_size
 
             netlib_wbool(exceeds_max)
@@ -155,64 +149,20 @@ if CLIENT then
             end
         end
 
-        netlib_wuint(_content_uid)
-
-        netlib_wuint(netlib_ruint())
-
         netlib_toserver()
     end)
 
     MDLStream.SendRequest = send_request
 
-    -- send_request("models/alyx.phy")
-    -- send_request("models/alyx.mdl")
-    -- send_request("models/dog.mdl")
+    send_request("models/alyx.phy")
+    send_request("models/alyx.mdl")
+    send_request("models/dog.mdl")
 else
     local delzma       = util.Decompress
     local tonumber     = tonumber
     local str_find     = string.find
     local netlib_send  = net.Send
 
-    local uid = uid or 1 -- included in msg
-    -- To ensure that every slice goes to a specific, corresponding field of a temp table
-    -- for later file build
-
-    local path_uid = path_uid or 1 -- included in msg
-    -- To ensure that every file is saved to a specific, corresponding location on server
-
-    --- Client Request:
-    -- writes path:string
-    -- writes clientside uid:int for content
-    --
-    --- Server receives Request:
-    -- reads path
-    -- writes mode:bool
-    -- writes uid:int for serverside slice
-    -- reads and writes uid for clientside content
-    -- ==> Server starts blink
-    --
-    --- Client receives Blink:
-    -- reads mode
-    -- reads uid(serverside slice) that reminds server
-    -- writes uid(serverside slice)
-    -- writes mode(slice type)
-    -- writes int content size
-    -- (optional if) reads int last endpos (content uid: uncertain position, may be after reading endpos)
-    -- writes data
-    -- (optional if) writes int endpos
-    -- writes uid(clientside content)
-    -- ==> Client starts slice
-    --
-    --- Server receives Slice:
-    -- reads uid(serverside slice)
-    -- reads bool(slice type)
-    -- reads int(content size)
-    -- reads data
-    -- (optional if) writes bool
-    --               writes int(serverside slice)
-    --               reads and writes uid(clientside content)
-    --               ==> Server starts blink, saves slice
-    -- (optional if) ==> Server saves slice then build file
     util.AddNetworkString("mdlstream_request")
     util.AddNetworkString("mdlstream_slice")
     util.AddNetworkString("mdlstream_svblink")
@@ -236,23 +186,21 @@ else
         return ret
     end
 
-    local context_paths = context_paths or {}
-    local slices_temp   = slices_temp or {}
+    local slice_temp = slice_temp or {}
 
     netlib_set_receiver("mdlstream_request", function(_, user)
-        path_uid = path_uid + 1
-        context_paths[path_uid] = net.ReadString()
-
         --- Whether checks file existence?
         --if not file.Exists(context_paths[uid], "GAME") then
-            uid = uid + 1
-            slices_temp[uid] = {}
             netlib_start("mdlstream_svblink")
-            netlib_wbool(false)
-            netlib_wuint(uid)
-            netlib_wuint(netlib_ruint())
 
-            netlib_wuint(path_uid)
+            netlib_wbool(false)
+
+            local _path = net.ReadString()
+            local _uid  = netlib_ruint()
+
+            slice_temp[_uid] = {[1] = {}, [2] = _path} -- DO THIS
+
+            netlib_wuint(_uid)
 
             netlib_send(user)
         --end
@@ -267,20 +215,17 @@ else
         if slice_type == false then
             local bytes
 
-            if #slices_temp[_uid] == 0 then
+            if #slice_temp[_uid][1] == 0 then
                 bytes = deserialize_table(delzma(content))
             else
-                slices_temp[_uid][#slices_temp[_uid] + 1] = content
+                slice_temp[_uid][1][#slice_temp[_uid][1] + 1] = content
 
-                bytes = deserialize_table(delzma(tblib_concat(slices_temp[_uid])))
+                bytes = deserialize_table(delzma(tblib_concat(slice_temp[_uid][1])))
             end
 
-            netlib_ruint()
-            local _path_uid = netlib_ruint()
+            file.CreateDir(string.GetPathFromFilename(slice_temp[_uid][2]))
 
-            file.CreateDir(string.GetPathFromFilename(context_paths[_path_uid]))
-
-            local _file = file.Open(context_paths[_path_uid], "wb", "DATA")
+            local _file = file.Open(slice_temp[_uid][2], "wb", "DATA")
 
             for i = 1, #bytes do
                 _file:WriteByte(bytes[i])
@@ -288,13 +233,13 @@ else
 
             _file:Close()
         elseif slice_type == true then
-            slices_temp[_uid][#slices_temp[_uid] + 1] = content
+            slice_temp[_uid][1][#slice_temp[_uid][1] + 1] = content
 
             netlib_start("mdlstream_svblink")
+
             netlib_wbool(true)
+
             netlib_wuint(_uid)
-            netlib_wuint(netlib_ruint())
-            netlib_wuint(netlib_ruint())
             netlib_wuint(netlib_ruint())
 
             netlib_send(user)
